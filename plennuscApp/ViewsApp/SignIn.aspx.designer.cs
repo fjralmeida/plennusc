5ed961d85998b7639ab1d56c39af81283fbff189
vallor-breno <vallorbreno@gmail.com>
2026-07-21 18:12:47 -0300

    fix: atualizar cores e títulos

--- plennuscApp/ViewsApp/SignIn.aspx.designer.cs
+++ plennuscApp/ViewsApp/SignIn.aspx.designer.cs
@@ -15,22 +15,13 @@ namespace appWhatsapp.Views
     {
 
         /// <summary>
-        /// Controle form1.
+        /// Controle form2.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Controle imgLogo.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image imgLogo;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
 
         /// <summary>
         /// Controle TextBoxEmail.
