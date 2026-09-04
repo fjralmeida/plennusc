93b3284256c93bf1ec90b86a251d74c4e8031172
leonardo.ambrosio <leonardo.ambrosio@vallorbeneficios.com.br>
2026-09-04 16:42:06 -0300

    Adicionar funcionalidade de relatório de atividades
    Implementadas novas classes para o relatório de atividades, incluindo `activityReportModels`, `activityReportService` e `dataActivityReport`. Atualizado o arquivo de configuração do projeto para incluir esses novos arquivos. Adicionada nova rota no `RouteConfig.cs` e criados arquivos ASPX com seus respectivos code-behind e designer para a página `activityReport.aspx`.

---
+++ Plennusc.Core/Service/ServiceGestao/CIDsService/activityReportService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plennusc.Core.Service.ServiceGestao.CIDsService
+{
+    internal class activityReportService
+    {
+    }
+}
